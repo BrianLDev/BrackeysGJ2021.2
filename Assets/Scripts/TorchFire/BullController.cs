558df3923d5126550d063e22eab245bc07f9a408
Brian Leip <brianleip@gmail.com>
2021-08-23 18:29:14 -0700

    Fixed spatialfree scene and player to work with input system changes

--- Assets/Scripts/TorchFire/BullController.cs
+++ Assets/Scripts/TorchFire/BullController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent( typeof(Animator), typeof(PlayerInput), typeof(PlayerInputHandler) )]
 public class BullController : MonoBehaviour {
     [SerializeField] float speed;
     private PlayerInputHandler inputHandler;
     private int animAttack, animIsEating, animIsWalking, animIsRunning, animIsDead;
 
     private void Awake() {
+        inputHandler = GetComponentInChildren<PlayerInputHandler>();
         inputHandler = gameObject.AddComponent<PlayerInputHandler>();
         animAttack = Animator.StringToHash("Attack");   // TODO - NEED ATTACK ANIMATION FOR BULL
         animIsEating = Animator.StringToHash("isEating");
