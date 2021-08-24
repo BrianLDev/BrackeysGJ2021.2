fbf393e856b37a9d42eaf94fd5110f203ab5ea61
Brian Leip <brianleip@gmail.com>
2021-08-23 17:39:55 -0700

    Updated to latest input system, created PlayerInputHandler to manage input

---
+++ Assets/Scripts/TorchFire/BullController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BullController : MonoBehaviour {
+    [SerializeField] float speed;
+    private PlayerInputHandler inputHandler;
+    private int animAttack, animIsEating, animIsWalking, animIsRunning, animIsDead;
+
+    private void Awake() {
+        inputHandler = gameObject.AddComponent<PlayerInputHandler>();
+        animAttack = Animator.StringToHash("Attack");   // TODO - NEED ATTACK ANIMATION FOR BULL
+        animIsEating = Animator.StringToHash("isEating");
+        animIsWalking = Animator.StringToHash("isWalking");
+        animIsRunning = Animator.StringToHash("isRunning");
+        animIsDead = Animator.StringToHash("isDead");
+    }
+
+    private void Update() {
+        if (inputHandler.isJumping) {
+            Debug.Log("Jumping! (handler)");
+        }
+    }
+}
---
+++ Assets/Scripts/TorchFire/PlayerInputHandler.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    public PlayerInput playerInput;
+    public Vector2 moveDirectionRaw { get; private set; }
+    public Vector2 lookDirectionRaw { get; private set; }
+    public Vector3 moveDirection { get; private set; }
+    public Vector3 lookDirection { get; private set; }
+    public bool isSprinting { get; private set; }
+    public bool isJumping { get; private set; }
+    public bool isAttacking { get; private set; }
+    public bool isDashing { get; private set; }
+    public bool isSpecialPerforming { get; private set; }
+    private string s_sprint = "Sprint";
+    private string s_jump = "Jump";
+    private string s_attack = "Attack";
+    private string s_dash = "Dash";
+    private string s_special = "Special";
+
+    private void Awake() {
+        playerInput = GetComponentInChildren<PlayerInput>();
+    }
+
+    public void OnMove(InputValue value) {
+        moveDirectionRaw = value.Get<Vector2>();
+        moveDirection = ConvertInputV2RawToV3(moveDirectionRaw);
+    }
+
+    public void OnLook(InputValue value) {
+        moveDirectionRaw = value.Get<Vector2>();
+        moveDirection = ConvertInputV2RawToV3(moveDirectionRaw);
+    }
+
+    public static Vector3 ConvertInputV2RawToV3(Vector2 inputRaw) {
+        return new Vector3(inputRaw.x, 0, inputRaw.y);
+    }
+
+    private void Update() {
+        if (playerInput.actions[s_sprint].IsPressed())
+            isSprinting = true;
+        else
+            isSprinting = false;
+
+        if (playerInput.actions[s_jump].IsPressed())
+            isJumping = true;
+        else
+            isJumping = false;
+
+        if (playerInput.actions[s_attack].IsPressed())
+            isAttacking = true;
+        else
+            isAttacking = false;
+
+        if (playerInput.actions[s_dash].IsPressed())
+            isDashing = true;
+        else
+            isDashing = false;
+
+        if (playerInput.actions[s_special].IsPressed())
+            isSpecialPerforming = true;
+        else
+            isSpecialPerforming = false;
+    }
+
+}
